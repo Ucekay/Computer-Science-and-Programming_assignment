36bd90f418bc00739dfeaf5ee3951750f01a1140
Ucekay <u331951c@ecs.osaka-u.ac.jp>
2023-06-15 14:46:47 +0900

    Create reidai9-1.c

---
+++ reidai9-1.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#define QUEUE_SIZE 5
+
+int gQueue[QUEUE_SIZE];
+int gQNum;
+
+void enqueue(int x) {
+	gQueue[gQNum++] = x;
+}
+
+int dequeue(void) {
+	int x;
+	int i;
+	x = gQueue[0];
+	for (i = 1; i < gQNum - 1; i ++) {
+		gQueue[i - 1] = gQueue[i];
+	}
+	gQNum --;
+	return x;
+}
+
+void initQueue() {
+	gQNum = 0;
+}
+
+void printQueue() {
+	printf("キューの中[");
+	for (int i = 0; i < gQNum; i ++) {
+		printf("%d", gQueue[i]);
+		if (i != gQNum - 1) {
+			printf(" ");
+		}
+	}
+	printf("]\n");
+}
+
+int main(void) {
+	int x;
+	int i;
+	
+	initQueue();
+	
+	for (i = 0; i <= QUEUE_SIZE - 1; i ++) {
+		printf("整数を入力してください。\n");
+		scanf("%d", &x);
+		enqueue(x);
+		printf("enqueue(%d): ", x);
+		printQueue();
+	}
+	
+	for (i = gQNum; i >= 0; i --) {
+		printf("dequeue(%d): ", dequeue());
+		printQueue();
+	}
+	
+	return 0;
+}
